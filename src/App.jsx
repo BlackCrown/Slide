bebb33af082cef38cb54c59cd6558be52241e603
Wagner Araujo <wagnercorp17@gmail.com>
2024-10-22 21:28:47 -0300

    Criado slide simples com funcao de passar e voltar slides, usando useState/ useEffect / useRef e translateX position

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,26 @@
 import React from 'react';
-import Produto from './produto';
+import Slide from './Slide';
 
-// Quando o usuário clicar em um dos botões, faça um fetch do produto clicado utilizando a api abaixo
-// https://ranekapi.origamid.dev/json/api/produto/notebook
-// https://ranekapi.origamid.dev/json/api/produto/smartphone
-// Mostre o nome e preço na tela (separe essa informação em um componente Produto.js)
-// Defina o produto clicado como uma preferência do usuário no localStorage
-// Quando o usuário entrar no site, se existe um produto no localStorage, faça o fetch do mesmo
-
-const App = () => {
-  // Definido o state, o mesmo recebe dados dos botoes da pagina e armazena
-  const [produto, setProduto] = React.useState(null);
-
-  // useEffect ira rodar ao iniciar a pagina
-  React.useEffect(() => {
-    // Ao abrir a pagina é salvo os dados do localStorage
-    const produtoLocal = window.localStorage.getItem('produto');
-    if (produto !== 'null') {
-      // caso localStorage nao esteja vazio, o dado é salvo como produto
-      setProduto(produtoLocal);
-    }
-  }, []);
-
-  // Sempre q o usuario clica nos botoes a var 'produto' é modificada, ativando assim este useEffect
-  React.useEffect(() => {
-    if (produto !== 'null') {
-      // Ao ser ativada a funcao salva a ultima escolha do usuário no localStorage
-      window.localStorage.setItem('produto', produto);
-    }
-  }, [produto]);
-
-  function handleClick(event) {
-    setProduto(event.target.innerText);
-    if (produto) localStorage.setItem('produto', produto);
-  }
-
-  /* A escolha de produto do usuario é enviada ao arquivo produto com uso de props*/
+function App() {
+  const slides = [
+    {
+      id: 'slide1',
+      text: 'slide1',
+    },
+    {
+      id: 'slide2',
+      text: 'slide2',
+    },
+    {
+      id: 'slide3',
+      text: 'slide3',
+    },
+  ];
   return (
-    <>
-      <h1>Preferência: {produto}</h1>
-      <button onClick={handleClick}>notebook</button>
-      <button onClick={handleClick}>smartphone</button>
-
-      <Produto produto={produto} />
-    </>
+    <div>
+      <Slide slides={slides} />
+    </div>
   );
-};
+}
 
 export default App;
